01fa08a486cf7764d4c094d4f9e95785fd7d01b8
Elsa <elsafirmaniar5@gmail.com>
2023-10-01 00:31:14 +0700

    Solve Problem 4

--- problem4/main.py
+++ problem4/main.py
@@ -2,6 +2,8 @@ def CekPrima(bilangan):
     prima = True 
     if bilangan <= 1 :
         prima = False
+    if bilangan == 2 :
+        prima = True
     for i in range(2, bilangan): 
         if (bilangan % i == 0): 
             prima = False 
@@ -31,12 +33,12 @@ def generate_primes_grid(width, height, start):
                 List_result.append(bil_prima[num])
             Urut = sorted(List_result)
         Hasil = ' '.join(map(str, Urut))
-        return Hasil
+        Hasil_akhir = f"{Hasil[0:5]}\n{Hasil[6:11]}\n{Hasil[12:17]}\n"
+        return Hasil_akhir
     else :
         List_result = []
         for i in range (height) :
             for j in range (width) :
-                start += 1
                 # temp_width =  (i * width)
                 # idx_result =  (j * temp_width) + temp_width 
                 num = start + i * width + j 
@@ -44,8 +46,10 @@ def generate_primes_grid(width, height, start):
                 num -= 1
                 bil_prima = get_prime(max)
                 List_result.append(bil_prima[num])
+                start += 1
         Hasil = ' '.join(map(str, List_result))
-        return Hasil
+        Hasil_akhir = f"{Hasil[0]}  {Hasil[2]}  {Hasil[4]}  {Hasil[6]} {Hasil[8:10]}\n{Hasil[11::]}\n"
+        return Hasil_akhir
 
 if __name__ == "__main__":
     print(generate_primes_grid(2, 3, 13))
